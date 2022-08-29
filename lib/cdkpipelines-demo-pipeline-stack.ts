696278e48f4ea296f6409c511cad7dd5f210c2fb
toyin <oginni@hotmail.com>
2022-08-30 00:37:08 +0100

    Update to owner and repo name

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -6,28 +6,28 @@ import { CodePipeline, CodePipelineSource, ShellStep } from "@aws-cdk/pipelines"
  * This is the pipeline stack
  */
 export class CdkpipelinesDemoPipelineStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      // The pipeline name
-      pipelineName: 'MyServicePipeline',
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            // The pipeline name
+            pipelineName: 'MyServicePipeline',
 
-       // How it will be built and synthesized
-       synth: new ShellStep('Synth', {
-         // Where the source can be found
-         input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
-         
-         // Install dependencies, build and run cdk synth
-         commands: [
-           'npm ci',
-           'npm run build',
-           'npx cdk synth'
-         ],
-       }),
-    });
+            // How it will be built and synthesized
+            synth: new ShellStep('Synth', {
+                // Where the source can be found
+                input: CodePipelineSource.gitHub('ToyinOA', 'cdk-pipelines-aws-demo'),
 
-    // This is where we add the application stages
-    // ...
-  }
+                // Install dependencies, build and run cdk synth
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npx cdk synth'
+                ],
+            }),
+        });
+
+        // This is where we add the application stages
+        // ...
+    }
 }
