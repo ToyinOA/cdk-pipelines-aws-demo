fdabff8e2899758542e0c7ddca2881df51b1e040
toyin <oginni@hotmail.com>
2022-08-30 02:54:10 +0100

    Add tests to PreProd stage

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -1,6 +1,7 @@
 import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
 import { CodePipeline, CodePipelineSource, ShellStep } from "@aws-cdk/pipelines";
 import { CdkpipelinesDemoStage } from './cdkpipelines-demo-stage';
+import { ShellScriptAction } from '@aws-cdk/pipelines';
 
 /**
  * The stack that defines the application pipeline
@@ -29,9 +30,28 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
         });
 
         // This is where we add the application stages
-        pipeline.addStage(new CdkpipelinesDemoStage(this, 'PreProd', {
+
+        // pipeline.addStage(new CdkpipelinesDemoStage(this, 'PreProd', {
+        //     env: { account: '644655966334', region: 'eu-west-2' }
+        // }));
+        const preprod = new CdkpipelinesDemoStage(this, 'PreProd', {
             env: { account: '644655966334', region: 'eu-west-2' }
-        }));
+        });
+        const preprodStage = pipeline.addStage(preprod, {
+            post: [
+                new ShellStep('TestService', {
+                    commands: [
+                        // Use 'curl' to GET the given URL and fail if it returns an error
+                        'curl -Ssf $ENDPOINT_URL',
+                    ],
+                    envFromCfnOutputs: {
+                        // Get the stack Output from the Stage and make it available in
+                        // the shell script as $ENDPOINT_URL.
+                        ENDPOINT_URL: preprod.urlOutput,
+                    },
+                }),
+            ],
+        });        
 
         pipeline.addStage(new CdkpipelinesDemoStage(this, 'Prod', {
             env: { account: '644655966334', region: 'eu-west-2' }
